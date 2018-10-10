6b3fbdf442ca4038d683f91a3e059e232b155329
dengcheng <644403982@qq.com>
2018-10-10 16:58:19 +0800

    add ssm-demo

---
+++ ssm-demo/src/main/java/com/fnl/ssmDemo/dao/domain/UserInfo.java
@@ -0,0 +1,103 @@
+package com.fnl.ssmDemo.dao.domain;
+
+public class UserInfo {
+    private Long userId;
+
+    private String name;
+
+    private String password;
+
+    private String account;
+
+    private String phone;
+
+    private String remark;
+
+    private Integer roleId;
+
+    private Short flag;
+
+    private Short deleteState;
+
+    private Long storeId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account == null ? null : account.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Short getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Short flag) {
+        this.flag = flag;
+    }
+
+    public Short getDeleteState() {
+        return deleteState;
+    }
+
+    public void setDeleteState(Short deleteState) {
+        this.deleteState = deleteState;
+    }
+
+    public Long getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Long storeId) {
+        this.storeId = storeId;
+    }
+}
---
+++ ssm-demo/src/main/java/com/fnl/ssmDemo/dao/mapper/UserInfoMapper.java
@@ -0,0 +1,18 @@
+package com.fnl.ssmDemo.dao.mapper;
+
+import com.fnl.ssmDemo.dao.domain.UserInfo;
+
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Long userId);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Long userId);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
